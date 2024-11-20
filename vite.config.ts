83006992d39da8c8c3394c0ac2aeb4e2ce727e3f
denis <denis@DENI>
2024-11-20 18:28:51 +0100

    Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/deni2312.github.io/"
+  base:'/deni2312.github.io/'
 })
-
