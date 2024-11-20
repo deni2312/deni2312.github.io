f226bf2ea40063179511944e3e99e29ac5648bbc
denis <denis@DENI>
2024-11-20 18:13:36 +0100

    Added deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // generate .vite/manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: 'src/main.tsx',
-    },
-  },
+  base:"/deni2312.github.io/"
 })
+
