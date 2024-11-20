fcefedcf4815f57401a47f357525cf9fdc53a26c
denis <denis@DENI>
2024-11-20 18:01:38 +0100

    Added mainfest

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: 'src/main.tsx',
+    },
+  },
 })
