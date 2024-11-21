d308c0a833da4d6dca26414b1e15047fd1e2fdd5
denis <denis@DENI>
2024-11-21 16:59:02 +0100

    Github Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
-import './App.css'
-import Site from './Site'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Site from "./Site";
+import ArticlesPage from "./ArticlesPage";
 
-function App() {
-  return <Site/>
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/articles">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* Use Routes and Route with the element prop */}
+        <Routes>
+          <Route path="/home" element={<Site />} />
+          <Route path="/articles" element={<ArticlesPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-
-export default App
---
+++ src/ArticlesPage.tsx
@@ -0,0 +1,7 @@
+
+function ArticlesPage() {
+  return (
+    <h1>Test</h1>);
+}
+
+export default ArticlesPage;
